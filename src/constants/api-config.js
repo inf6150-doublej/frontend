901123506f71e8c46b687044dadf2c1645029fd0
Steve Gadoury <steve.gadoury@gmail.com>
2019-05-21 22:00:01 -0400

    change server url

--- src/constants/api-config.js
+++ src/constants/api-config.js
@@ -6,7 +6,7 @@ const hostname = window && window.location && window.location.hostname;
 if(/localhost/.test(hostname)) {
     backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:5000';
 } else {
-    backendHost = `${hostname}`;
+    backendHost = 'https://bookingexpert-api.herokuapp.com/';
 }
 
 export const API_ROOT = `${backendHost}`;
