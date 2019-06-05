50ec68d841bd13fcd430e9afc24760fa8d0aaf34
Christophe <chhubr@hotmail.com>
2019-06-05 19:07:05 -0400

    Fix affichage dans l'equipement

--- src/components/pure/Room.jsx
+++ src/components/pure/Room.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Media from 'react-bootstrap/Media';
 import logo from '../../img/BE2.png';
-
+import getType from '../../utils/utils';
 
 { /*} Figure out how to display the logo and title at the top { */ }
+
+
 const Room = (props) => {
+  const equipment = (equip)=>{
+    const liste = [];
+    for(var e in equip){
+      if(equip[e] === 1)liste.push(e);
+    }
+      
+    
+    return <ul>
+          {liste.join(", ")}
+          </ul>
+  }
   const { room, onReservation } = props;
   return (
     <div className="room-container">
@@ -20,11 +33,11 @@ const Room = (props) => {
           <Media.Body>
             <br></br>
             <h5>#{room.id} - {room.name}</h5>
-            <li>{room.type}</li> {/*} Type de salle? Devrait=on ne pas plutot y voir le type plutot qu'un chiffre? { */}
+            <li>{getType(room.type)}</li> {/*} Type de salle? Devrait=on ne pas plutot y voir le type plutot qu'un chiffre? { */}
             <li>Description: {room.description}</li>
             <li>Maximum capacity: {room.capacity}</li>
             <li>{room.reservation_id}</li> {/* } Pourquoi on display ca? {*/}
-            <li>Equipment available: {room.equipment_id}</li> {/*} Je ne comprends pas c'est quoi cet ID, devrait-on ne pas plutot avoir une liste des equipements offerts?{ */}
+            <li>Equipment available: {equipment(room.equipment)}</li> {/*} Je ne comprends pas c'est quoi cet ID, devrait-on ne pas plutot avoir une liste des equipements offerts?{ */}
           </Media.Body>
         </Media>
       </ul>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,3 +2,26 @@ export function isValidEmail (email) {
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(String(email).toLowerCase());
   }
+
+export default function getType (type){
+  switch(type) {
+    case 1:
+      return 'Arena';
+    case 2:
+      return 'Auditorium';
+    case 3:
+      return 'Bar';
+    case 4:
+      return 'University';
+    case 5:
+      return 'Theatre';
+    case 6:
+      return 'Cultural center';
+    case 7:
+      return 'House';
+    case 8:
+      return 'Outdoor';
+    default:
+      return '';
+  }              
+}
